649c92c2c33c052ee51ecd699051e64c4710ce24
julobrat <julobrat@gmail.com>
2024-12-28 18:09:01 +0100

    feat: working camera with OrthogonalProjection

--- src/bella/ui/camera.rs
+++ src/bella/ui/camera.rs
@@ -1,12 +1,15 @@
-use bevy::prelude::*;
-use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+use bevy::{
+    input::mouse::{AccumulatedMouseMotion, AccumulatedMouseScroll},
+    prelude::*,
+    render::camera::ScalingMode,
+};
 
 pub struct MyCameraPlugin;
 
 impl Plugin for MyCameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(PanOrbitCameraPlugin)
-            .add_systems(Startup, spawn_camera_and_light);
+        app.add_systems(Startup, spawn_camera_and_light)
+            .add_systems(Update, (camera_movement, camera_zoom, rotate_camera));
     }
 }
 
@@ -14,18 +17,22 @@ impl Plugin for MyCameraPlugin {
 struct MyGameCameraMarker;
 
 fn spawn_camera_and_light(mut cmd: Commands) {
-    let mut projection = OrthographicProjection::default_3d();
-    projection.scale = 0.5;
+    let projection = OrthographicProjection {
+        scale: 0.5,
+        near: -1_000_000.0,
+        far: 1_000_000.0,
+        viewport_origin: Vec2::new(0.5, 0.5),
+        scaling_mode: ScalingMode::WindowSize,
+        area: Rect::new(-1.0, -1.0, 1.0, 1.0),
+    };
 
     cmd.spawn((
         MyGameCameraMarker,
         Camera3d::default(),
-        PanOrbitCamera {
-            allow_upside_down: true,
-            focus: Vec3::new(0., 300., 0.),
-            ..Default::default()
-        },
-        Transform::from_xyz(0.0, 0.0, 1600.0),
+        Transform::from_xyz(0.0, 0.0, 200.0)
+            .looking_to(Vec3::new(0.0, 0.3, -0.7), Vec3::new(0.0, 0.3, 0.7)),
+        Projection::from(projection.clone()),
+        projection,
     ));
 
     cmd.spawn((
@@ -33,10 +40,79 @@ fn spawn_camera_and_light(mut cmd: Commands) {
         PointLight {
             color: Color::WHITE,
             intensity: 100_000_000_000.0,
-            range: 200_000.0,
-            radius: 10.0,
+            range: 2_000_000_000.0,
+            radius: 100.0,
             shadows_enabled: true,
             ..Default::default()
         },
     ));
+
+    cmd.spawn((
+        Transform::from_xyz(3800.0, 2400.0, 2_000.0),
+        PointLight {
+            color: Color::WHITE,
+            intensity: 100_000_000_000.0,
+            range: 2_000_000_000.0,
+            radius: 100.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+    ));
+}
+
+fn camera_movement(
+    camera: Single<(&mut Transform, &Projection), With<MyGameCameraMarker>>,
+    mouse_motion: Res<AccumulatedMouseMotion>,
+    mouse_button: Res<ButtonInput<MouseButton>>,
+) {
+    if !mouse_button.pressed(MouseButton::Right) {
+        return;
+    }
+
+    let (mut transform, projection) = camera.into_inner();
+
+    if let Projection::Orthographic(ortographic_projection) = projection {
+        let mut move_by = mouse_motion.delta.extend(0.0);
+        move_by.x *= -1.0;
+        move_by *= ortographic_projection.scale;
+
+        transform.translation += move_by;
+    }
+}
+
+fn camera_zoom(
+    mut projection: Single<&mut Projection, With<MyGameCameraMarker>>,
+    mouse_wheel_input: Res<AccumulatedMouseScroll>,
+) {
+    if let Projection::Orthographic(ortographic_projection) = projection.as_mut() {
+        let mut log_scale = ortographic_projection.scale.ln();
+        log_scale -= 0.1 * mouse_wheel_input.delta.y;
+        ortographic_projection.scale = log_scale.exp();
+    }
+}
+
+// TODO: remove if not needed for debug
+fn rotate_camera(
+    input: Res<ButtonInput<KeyCode>>,
+    mut camera_q: Query<&mut Transform, With<MyGameCameraMarker>>,
+) {
+    let mut translation = camera_q.single_mut();
+
+    let sign = if input.pressed(KeyCode::ShiftLeft) {
+        -1.
+    } else {
+        1.
+    };
+
+    if input.pressed(KeyCode::KeyX) {
+        translation.rotate_x(sign * 1.0_f32.to_radians());
+    }
+
+    if input.pressed(KeyCode::KeyY) {
+        translation.rotate_y(sign * 1.0_f32.to_radians());
+    }
+
+    if input.pressed(KeyCode::KeyZ) {
+        translation.rotate_z(sign * 1.0_f32.to_radians());
+    }
 }
