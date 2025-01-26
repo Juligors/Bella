75b53671fadf06dea374315db989d0f090b1849a
julobrat <julobrat@gmail.com>
2025-01-26 12:31:43 +0100

    feat(partial): remove distribution file, move content to config

--- src/bella/config.rs
+++ src/bella/config.rs
@@ -1,8 +1,10 @@
+use super::organism::gene::Gene;
 use bevy::prelude::*;
 use config::Config;
+use rand::{rngs::ThreadRng, thread_rng, Rng};
+use rand_distr::{Normal, Uniform, WeightedIndex};
 use serde::Deserialize;
-
-use super::distribution::{BooleanDistribution, ContinuousDistribution, DiscreteDistribution};
+use std::cell::RefCell;
 
 pub struct ConfigPlugin;
 
@@ -14,49 +16,49 @@ impl Plugin for ConfigPlugin {
 
 fn load_config(mut cmd: Commands) {
     let organism_config = Config::builder()
-        .add_source(config::File::with_name("config/organisms.toml"))
+        .add_source(config::File::with_name("config/organisms.yaml"))
         .build()
         .expect("Can't read organism configuration!")
         .try_deserialize::<OrganismConfig>()
         .expect("Can't deserialize organism config to config struct!");
 
     let animal_config = Config::builder()
-        .add_source(config::File::with_name("config/animals.toml"))
+        .add_source(config::File::with_name("config/animals.yaml"))
         .build()
         .expect("Can't read animal configuration!")
         .try_deserialize::<AnimalConfig>()
         .expect("Can't deserialize animal config to config struct!");
 
     let plant_config = Config::builder()
-        .add_source(config::File::with_name("config/plants.toml"))
+        .add_source(config::File::with_name("config/plants.yaml"))
         .build()
         .expect("Can't read plant configuration!")
         .try_deserialize::<PlantConfig>()
         .expect("Can't deserialize plant config to config struct!");
 
     let terrain_config = Config::builder()
-        .add_source(config::File::with_name("config/terrain.toml"))
+        .add_source(config::File::with_name("config/terrain.yaml"))
         .build()
         .expect("Can't read terrain configuration!")
         .try_deserialize::<TerrainConfig>()
         .expect("Can't deserialize terrain config to config struct!");
 
     let time_config = Config::builder()
-        .add_source(config::File::with_name("config/time.toml"))
+        .add_source(config::File::with_name("config/time.yaml"))
         .build()
         .expect("Can't read time configuration!")
         .try_deserialize::<TimeConfig>()
         .expect("Can't deserialize time config to config struct!");
 
     let environment_config = Config::builder()
-        .add_source(config::File::with_name("config/environment.toml"))
+        .add_source(config::File::with_name("config/environment.yaml"))
         .build()
         .expect("Can't read environment configuration!")
         .try_deserialize::<EnvironmentConfig>()
         .expect("Can't deserialize environment config to config struct!");
 
     let data_collection_config = Config::builder()
-        .add_source(config::File::with_name("config/data_collection.toml"))
+        .add_source(config::File::with_name("config/data_collection.yaml"))
         .build()
         .expect("Can't read data collection configuration!")
         .try_deserialize::<DataCollectionConfig>()
@@ -86,14 +88,20 @@ pub struct SimConfig {
 
 #[derive(Debug, Deserialize)]
 pub struct OrganismConfig {
-    /// percentage of energy lost by carcass every hour
-    pub carcass_energy_decay: f32,
+    pub max_health_gene_config: UnsignedFloatGeneConfig,
+    pub max_active_energy_gene_config: UnsignedFloatGeneConfig,
 
-    pub max_active_energy_dist: ContinuousDistribution,
+    pub energy_to_survive_per_mass_unit_gene_config: UnsignedFloatGeneConfig,
+    pub reproduction_energy_cost_gene_config: UnsignedFloatGeneConfig,
+
+    pub maturity_age_gene_config: UnsignedIntGeneConfig,
+    pub reproduction_cooldown_gene_config: UnsignedIntGeneConfig,
+
     pub starting_mass_dist: ContinuousDistribution,
-    pub energy_per_mass_unit_dist: ContinuousDistribution,
-    pub energy_consumption_per_mass_unit_dist: ContinuousDistribution,
 
+    pub offspring_spawn_range: f32,
+    pub max_energy_consumption_per_mass_unit: f32,
+    pub carcass_mass_decay_percentage: f32,
 }
 
 #[derive(Debug, Deserialize)]
@@ -108,23 +116,19 @@ pub struct AnimalConfig {
     pub attack_range_dist: ContinuousDistribution,
     pub attack_damage_dist: ContinuousDistribution,
 
-    pub development_time: i8,
-    pub waiting_for_reproduction_time: i8,
-    pub reproduction_range: f32,
-
+    // TODO: to już jest w Organism
+    // pub development_time: i8,
+    // pub waiting_for_reproduction_time: i8,
     pub carnivores_to_herbivores_ratio: f32,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct PlantConfig {
+    pub energy_production_from_solar_efficiency_gene_config: UnsignedFloatGeneConfig,
+    pub pollination_range_gene_config: UnsignedFloatGeneConfig,
+
     pub group_spawn_on_grass_chance: BooleanDistribution,
     pub group_size_dist: DiscreteDistribution,
-    pub max_health_dist: ContinuousDistribution,
-    pub energy_efficiency_production_from_solar_dist: ContinuousDistribution,
-    
-    pub reproduction_range: f32,
-    pub development_time: i8,
-    pub waiting_for_reproduction_time: i8,
 }
 
 #[derive(Debug, Deserialize)]
@@ -145,7 +149,7 @@ pub struct TimeConfig {
 #[derive(Debug, Deserialize)]
 pub struct EnvironmentConfig {
     pub starting_hour: u8,
-    pub sun_energy_output: f32,
+    pub sun_energy_output_per_tile: f32,
     pub sun_energy_output_per_plant: f32,
     pub sun_day_energy_ratio: f32,
     pub sun_night_energy_ratio: f32,
@@ -156,4 +160,122 @@ pub struct DataCollectionConfig {
     pub directory: String,
     pub plants_filename: String,
     pub animals_filename: String,
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+thread_local! {
+    static RNG: RefCell<ThreadRng> = RefCell::new(thread_rng());
+}
+
+#[derive(Debug, Deserialize, Clone, Copy)]
+#[serde(rename_all = "lowercase")]
+pub struct UnsignedFloatGeneConfig {
+    pub multiplier: f32,
+    pub offset: f32,
+}
+
+#[derive(Debug, Deserialize, Clone, Copy)]
+#[serde(rename_all = "lowercase")]
+pub struct UnsignedIntGeneConfig {
+    pub max_value: u32,
+    pub min_value: u32,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "lowercase")]
+pub enum BooleanDistribution {
+    Chance { chance: f32 },
+}
+
+impl BooleanDistribution {
+    pub fn happened(&self) -> bool {
+        RNG.with(|rng| {
+            let mut rng = rng.borrow_mut();
+
+            match self {
+                BooleanDistribution::Chance { chance } => rng.gen_bool(*chance as f64),
+            }
+        })
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "lowercase")]
+pub enum DiscreteDistribution {
+    Range {
+        min: i32,
+        max: i32,
+    },
+    Choice {
+        choices: Vec<i32>,
+    },
+    WeightedChoice {
+        choices: Vec<i32>,
+        weights: Vec<f32>,
+    },
+}
+
+impl DiscreteDistribution {
+    pub fn sample(&self) -> i32 {
+        RNG.with(|rng| {
+            let mut rng = rng.borrow_mut();
+
+            match self {
+                DiscreteDistribution::Range { min, max } => rng.gen_range(*min..*max + 1),
+                DiscreteDistribution::Choice { choices } => {
+                    choices[rng.gen_range(0..choices.len())]
+                }
+                DiscreteDistribution::WeightedChoice { choices, weights } => {
+                    let dist = WeightedIndex::new(weights).expect("Invalid weights");
+
+                    choices[rng.sample(dist)]
+                }
+            }
+        })
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "lowercase")]
+pub enum ContinuousDistribution {
+    Normal {
+        mean: f32,
+        std: f32,
+        min: Option<f32>,
+        max: Option<f32>,
+    },
+    Uniform {
+        min: f32,
+        max: f32,
+    },
+}
+
+impl ContinuousDistribution {
+    pub fn sample(&self) -> f32 {
+        RNG.with(|rng| {
+            let mut rng = rng.borrow_mut();
+
+            match self {
+                ContinuousDistribution::Normal {
+                    mean,
+                    std,
+                    min,
+                    max,
+                } => {
+                    let result = rng.sample(
+                        Normal::new(*mean, *std).expect("Failed to create standard distribution"),
+                    );
+
+                    result.clamp(min.unwrap_or(f32::MIN), max.unwrap_or(f32::MAX))
+                }
+                ContinuousDistribution::Uniform { min, max } => {
+                    rng.sample(Uniform::new(*min, *max))
+                }
+            }
+        })
+    }
 }
