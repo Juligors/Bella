ec75ef70191c3a31c440db3649878f70b2813577
julobrat <julobrat@gmail.com>
2025-01-26 12:32:29 +0100

    commented out code for TODO for logging

--- src/bella/window.rs
+++ src/bella/window.rs
@@ -1,7 +1,19 @@
+use std::{fs::File, sync::Mutex};
+
 use bevy::{
     core::TaskPoolThreadAssignmentPolicy,
+    log::{
+        self,
+        tracing_subscriber::{
+            self,
+            fmt::{self, format::FmtSpan},
+            FmtSubscriber, Layer,
+        },
+        BoxedLayer, LogPlugin,
+    },
     prelude::*,
     tasks::available_parallelism,
+    utils::tracing::{self, level_filters::LevelFilter, Subscriber},
     window::{CursorGrabMode, PresentMode, WindowLevel, WindowTheme},
 };
 
@@ -14,11 +26,11 @@ impl Plugin for MyWindowPlugin {
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Bella".into(),
-                        resolution: (1000., 700.).into(),
+                        resolution: (1400., 700.).into(),
                         present_mode: PresentMode::AutoVsync,
                         window_theme: Some(WindowTheme::Dark),
                         window_level: WindowLevel::AlwaysOnTop,
-                        position: WindowPosition::At((400, 100).into()),
+                        position: WindowPosition::At((75, 100).into()),
                         ..default()
                     }),
                     ..default()
@@ -35,7 +47,18 @@ impl Plugin for MyWindowPlugin {
                         },
                         ..default()
                     },
+                })
+                .set(LogPlugin {
+                    // filter: "info,wgpu_core=error,wgpu_hal=error,bevy_render=error,bevy_ecs=trace,bella=debug".into(),
+                    filter: "error,bella=debug,bevy_ecs=trace".into(),
+                    level: log::Level::DEBUG,
+                    // TODO(LOGS): might be cool to customize it to save to file, but would need to filter better and fix formatting issues
+                    // custom_layer: custom_logger_layer,
+                    ..Default::default()
                 }),
+            // TODO(LOGS)
+            // .disable::<bevy::log::LogPlugin>()
+            // .build(),
             // bevy::diagnostic::LogDiagnosticsPlugin::default(),
             // bevy::diagnostic::FrameTimeDiagnosticsPlugin::default(),
         ))
@@ -66,3 +89,57 @@ pub fn close_on_esc(
         }
     }
 }
+
+struct CustomLoggingLayer;
+
+impl<S: Subscriber> Layer<S> for CustomLoggingLayer {
+    fn on_event(
+        &self,
+        event: &tracing::Event<'_>,
+        _ctx: bevy::log::tracing_subscriber::layer::Context<'_, S>,
+    ) {
+        println!("Got event!");
+        println!("  level={}", event.metadata().level());
+        println!("  target={}", event.metadata().target());
+        println!("  name={}", event.metadata().name());
+    }
+}
+
+fn custom_logger_layer(_app: &mut App) -> Option<BoxedLayer> {
+    // return None;
+
+    let file = File::create("debug_logs.txt").expect("Failed to create log file");
+    let file = Mutex::new(file); // Mutex is needed because the writer may be accessed concurrently.
+
+    let file_layer = fmt::layer()
+        .with_writer(file)
+        .with_span_events(FmtSpan::CLOSE) // Log spans on close
+        .with_target(false) // Don't include target (module path) in file
+        .with_filter(LevelFilter::DEBUG); // Only capture DEBUG+ logs
+
+    let console_layer = fmt::layer()
+        .with_writer(std::io::stdout)
+        // .event_format(fmt::format().format(custom_format))
+        .with_filter(LevelFilter::INFO);
+
+    // Additional custom layer to hook into events (optional)
+    // let custom_layer = CustomLoggingLayer.boxed();
+
+    // Some(Box::new(vec![file_layer.boxed(), console_layer.boxed(), custom_layer]))
+    Some(Box::new(vec![file_layer.boxed(), console_layer.boxed()]))
+}
+
+// fn custom_format(
+//     writer: &mut dyn std::fmt::Write,
+//     event: &tracing::Event<'_>,
+//     _: impl tracing_subscriber::fmt::format::FormatFields<'_>,
+// ) -> std::fmt::Result {
+//     let metadata = event.metadata();
+//     writeln!(
+//         writer,
+//         "[{}] {}: {}",
+//         metadata.level(),
+//         metadata.target(),
+//         metadata.name().unwrap_or("unknown_event"),
+//     )
+// }
